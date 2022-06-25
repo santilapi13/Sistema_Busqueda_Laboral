7021e7f8dea11b1aa445732ff04cd8dccbda6e14
lunalauti <102366234+lunalauti@users.noreply.github.com>
2022-06-25 18:19:37 -0300

    Agregados pesos al formulario

--- src/vista/VFormulario.java
+++ src/vista/VFormulario.java
@@ -2,37 +2,26 @@ package vista;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.Window.Type;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.FlowLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import javax.swing.JTextField;
-import javax.swing.JRadioButton;
-import javax.swing.border.TitledBorder;
-
-import modelo.Empleado;
-import modelo.Empleador;
-
-import javax.swing.border.EtchedBorder;
-import java.awt.Color;
 import java.awt.Font;
-import javax.swing.BoxLayout;
-import javax.swing.ButtonGroup;
-
+import java.awt.GridLayout;
 import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseListener;
-import java.util.ArrayList;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class VFormulario extends JFrame implements MouseListener {
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+public class VFormulario extends JFrame implements MouseListener, KeyListener {
 
 	private JPanel contentPane;
 	private JPanel panelBotones;
@@ -96,20 +85,57 @@ public class VFormulario extends JFrame implements MouseListener {
 	private JRadioButton rdbtnMucha;
 	private ActionListener actionlistener;
 
-	//Para que se pueda seleccionar un solo rdbutton de cada aspecto
+	// Para que se pueda seleccionar un solo rdbutton de cada aspecto
 	private ButtonGroup bgLocacion = new ButtonGroup();
 	private ButtonGroup bgRemuneracion = new ButtonGroup();
 	private ButtonGroup bgRangoEtario = new ButtonGroup();
 	private ButtonGroup bgCargaHoraria = new ButtonGroup();
 	private ButtonGroup bgExperiencia = new ButtonGroup();
 	private ButtonGroup bgPuesto = new ButtonGroup();
-	private ButtonGroup bgEstudios= new ButtonGroup();
+	private ButtonGroup bgEstudios = new ButtonGroup();
+	private JPanel panelPesos;
+	private JPanel panel_1;
+	private JPanel panel_3;
+	private JLabel lblNewLabel_8;
+	private JTextField textPLocacion;
+	private JPanel panel_4;
+	private JLabel lblNewLabel_9;
+	private JTextField textPRemuneracion;
+	private JPanel panel_5;
+	private JLabel lblNewLabel_10;
+	private JTextField textPCargaH;
+	private JPanel panel_6;
+	private JLabel lblNewLabel_11;
+	private JTextField textPPuesto;
+	private JPanel panel_7;
+	private JLabel lblNewLabel_12;
+	private JTextField textPRangoE;
+	private JPanel panel_8;
+	private JLabel lblNewLabel_13;
+	private JTextField textPExperiencia;
+	private JPanel panel_9;
+	private JLabel lblNewLabel_14;
+	private JTextField textPEstudios;
+	private JLabel lblNewLabel_6;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VFormulario frame = new VFormulario();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 	
 	public VFormulario() {
 		setTitle("SISTEMA DE BUSQUEDA LABORAL");
 		setAlwaysOnTop(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 495, 473);
+		setBounds(100, 100, 532, 473);
 		this.contentPane = new JPanel();
 		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(this.contentPane);
@@ -143,6 +169,7 @@ public class VFormulario extends JFrame implements MouseListener {
 		this.panelCant.add(this.labelCant);
 
 		this.textCant = new JTextField();
+		this.textCant.addKeyListener(this);
 		this.textCant.setHorizontalAlignment(SwingConstants.LEFT);
 		this.textCant.setEnabled(false);
 		this.panelCant.add(this.textCant);
@@ -174,13 +201,13 @@ public class VFormulario extends JFrame implements MouseListener {
 		this.rdbtnHomeOffice.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_1.add(this.rdbtnHomeOffice);
 		this.bgLocacion.add(rdbtnHomeOffice);
-		
+
 		this.rdbtnPresencial = new JRadioButton("Presencial");
 		this.rdbtnPresencial.addMouseListener(this);
 		this.rdbtnPresencial.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_1.add(this.rdbtnPresencial);
 		this.bgLocacion.add(rdbtnPresencial);
-		
+
 		this.rdbtnIndistinto = new JRadioButton("Indistinto");
 		this.rdbtnIndistinto.addMouseListener(this);
 		this.rdbtnIndistinto.setFont(new Font("Arial", Font.PLAIN, 12));
@@ -209,19 +236,19 @@ public class VFormulario extends JFrame implements MouseListener {
 		this.rdbtnV1.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_2.add(this.rdbtnV1);
 		this.bgRemuneracion.add(rdbtnV1);
-		
+
 		this.rdbtnV1V2 = new JRadioButton("Entre V1 y V2");
 		this.rdbtnV1V2.addMouseListener(this);
 		this.rdbtnV1V2.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_2.add(this.rdbtnV1V2);
 		this.bgRemuneracion.add(rdbtnV1V2);
-		
+
 		this.rdbtnV2 = new JRadioButton("Mas de V2");
 		this.rdbtnV2.addMouseListener(this);
 		this.rdbtnV2.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_2.add(this.rdbtnV2);
 		this.bgRemuneracion.add(rdbtnV2);
-		
+
 		this.panelCargaHoraria = new JPanel();
 		this.panelRadio.add(this.panelCargaHoraria);
 		this.panelCargaHoraria.setLayout(new GridLayout(2, 0, 0, 0));
@@ -244,19 +271,19 @@ public class VFormulario extends JFrame implements MouseListener {
 		this.rdbtnMedia.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_3.add(this.rdbtnMedia);
 		this.bgCargaHoraria.add(rdbtnMedia);
-		
+
 		this.rdbtnVCompleta = new JRadioButton("Completa");
 		this.rdbtnVCompleta.addMouseListener(this);
 		this.rdbtnVCompleta.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_3.add(this.rdbtnVCompleta);
 		this.bgCargaHoraria.add(rdbtnVCompleta);
-		
+
 		this.rdbtnExtendida = new JRadioButton("Extendida");
 		this.rdbtnExtendida.addMouseListener(this);
 		this.rdbtnExtendida.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_3.add(this.rdbtnExtendida);
 		this.bgCargaHoraria.add(rdbtnExtendida);
-		
+
 		this.panelPuesto = new JPanel();
 		this.panelRadio.add(this.panelPuesto);
 		this.panelPuesto.setLayout(new GridLayout(2, 0, 0, 0));
@@ -273,25 +300,25 @@ public class VFormulario extends JFrame implements MouseListener {
 		this.radiobtn_4 = new JPanel();
 		this.panelPuesto.add(this.radiobtn_4);
 		this.radiobtn_4.setLayout(new GridLayout(0, 3, 0, 0));
-		
+
 		this.rdbtnJunior = new JRadioButton("Junior");
 		this.rdbtnJunior.addMouseListener(this);
 		this.rdbtnJunior.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_4.add(this.rdbtnJunior);
 		this.bgPuesto.add(rdbtnJunior);
-		
+
 		this.rdbtnSenior = new JRadioButton("Senior");
 		this.rdbtnSenior.addMouseListener(this);
 		this.rdbtnSenior.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_4.add(this.rdbtnSenior);
 		this.bgPuesto.add(rdbtnSenior);
-		
+
 		this.rdbtnManagement = new JRadioButton("Management");
 		this.rdbtnManagement.addMouseListener(this);
 		this.rdbtnManagement.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_4.add(this.rdbtnManagement);
 		this.bgPuesto.add(rdbtnManagement);
-		
+
 		this.panelRangoEtario = new JPanel();
 		this.panelRadio.add(this.panelRangoEtario);
 		this.panelRangoEtario.setLayout(new GridLayout(2, 0, 0, 0));
@@ -316,21 +343,21 @@ public class VFormulario extends JFrame implements MouseListener {
 		this.rdbtnE1.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_5.add(this.rdbtnE1);
 		this.bgRangoEtario.add(rdbtnE1);
-		
+
 		this.rdbtnE2 = new JRadioButton("Entre 40 y 50");
 		this.rdbtnE2.addMouseListener(this);
 		this.rdbtnE2.setEnabled(false);
 		this.rdbtnE2.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_5.add(this.rdbtnE2);
 		this.bgRangoEtario.add(rdbtnE2);
-		
+
 		this.rdbtnE3 = new JRadioButton("Mas de 50");
 		this.rdbtnE3.addMouseListener(this);
 		this.rdbtnE3.setEnabled(false);
 		this.rdbtnE3.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_5.add(this.rdbtnE3);
 		this.bgRangoEtario.add(rdbtnE3);
-		
+
 		this.panelExperiencia = new JPanel();
 		this.panelRadio.add(this.panelExperiencia);
 		this.panelExperiencia.setLayout(new GridLayout(2, 0, 0, 0));
@@ -353,19 +380,19 @@ public class VFormulario extends JFrame implements MouseListener {
 		this.rdbtnNada.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_6.add(this.rdbtnNada);
 		this.bgExperiencia.add(rdbtnNada);
-		
+
 		this.rdbtnEMedia = new JRadioButton("Media");
 		this.rdbtnEMedia.addMouseListener(this);
 		this.rdbtnEMedia.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_6.add(this.rdbtnEMedia);
 		this.bgExperiencia.add(rdbtnEMedia);
-		
+
 		this.rdbtnMucha = new JRadioButton("Mucha");
 		this.rdbtnMucha.addMouseListener(this);
 		this.rdbtnMucha.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn_6.add(this.rdbtnMucha);
 		this.bgExperiencia.add(rdbtnMucha);
-		
+
 		JPanel panelRemuneracion9 = new JPanel();
 		this.panelRadio.add(panelRemuneracion9);
 		panelRemuneracion9.setLayout(new GridLayout(2, 0, 0, 0));
@@ -388,19 +415,19 @@ public class VFormulario extends JFrame implements MouseListener {
 		this.rdbtnPrimario.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn.add(this.rdbtnPrimario);
 		this.bgEstudios.add(rdbtnPrimario);
-		
+
 		this.rdbtnSecundario = new JRadioButton("Secundario");
 		this.rdbtnSecundario.addMouseListener(this);
 		this.rdbtnSecundario.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn.add(this.rdbtnSecundario);
 		this.bgEstudios.add(rdbtnSecundario);
-		
+
 		this.rdbtnTerciario = new JRadioButton("Terciario");
 		this.rdbtnTerciario.addMouseListener(this);
 		this.rdbtnTerciario.setFont(new Font("Arial", Font.PLAIN, 12));
 		this.radiobtn.add(this.rdbtnTerciario);
 		this.bgEstudios.add(rdbtnTerciario);
-		
+
 		this.panel = new JPanel();
 		this.contentPane.add(this.panel, BorderLayout.NORTH);
 
@@ -408,44 +435,170 @@ public class VFormulario extends JFrame implements MouseListener {
 		this.labelTitulo.setFont(new Font("Arial", Font.BOLD, 20));
 		this.labelTitulo.setHorizontalAlignment(SwingConstants.LEFT);
 		this.panel.add(this.labelTitulo);
+
+		this.panelPesos = new JPanel();
+		this.contentPane.add(this.panelPesos, BorderLayout.EAST);
+		this.panelPesos.setLayout(new BorderLayout(0, 0));
+
+		this.panel_1 = new JPanel();
+		this.panelPesos.add(this.panel_1);
+		this.panel_1.setLayout(new GridLayout(7, 0, 0, 0));
+
+		this.panel_3 = new JPanel();
+		this.panel_1.add(this.panel_3);
+		this.panel_3.setLayout(new GridLayout(2, 1, 0, 0));
+
+		this.lblNewLabel_8 = new JLabel("PESOS");
+		this.lblNewLabel_8.setFont(new Font("Tahoma", Font.BOLD, 13));
+		this.lblNewLabel_8.setHorizontalAlignment(SwingConstants.CENTER);
+		this.panel_3.add(this.lblNewLabel_8);
+
+		this.textPLocacion = new JTextField();
+		this.textPLocacion.addKeyListener(this);
+		this.textPLocacion.setHorizontalAlignment(SwingConstants.CENTER);
+		this.textPLocacion.setText("1");
+		this.panel_3.add(this.textPLocacion);
+		this.textPLocacion.setColumns(10);
+
+		this.panel_4 = new JPanel();
+		this.panel_1.add(this.panel_4);
+		this.panel_4.setLayout(new GridLayout(2, 1, 0, 0));
+
+		this.lblNewLabel_9 = new JLabel("");
+		this.panel_4.add(this.lblNewLabel_9);
+
+		this.textPRemuneracion = new JTextField();
+		this.textPRemuneracion.addKeyListener(this);
+		this.textPRemuneracion.setHorizontalAlignment(SwingConstants.CENTER);
+		this.textPRemuneracion.setText("1");
+		this.textPRemuneracion.setColumns(10);
+		this.panel_4.add(this.textPRemuneracion);
+
+		this.panel_5 = new JPanel();
+		this.panel_1.add(this.panel_5);
+		this.panel_5.setLayout(new GridLayout(2, 1, 0, 0));
+
+		this.lblNewLabel_10 = new JLabel("");
+		this.panel_5.add(this.lblNewLabel_10);
+
+		this.textPCargaH = new JTextField();
+		this.textPCargaH.addKeyListener(this);
+		this.textPCargaH.setHorizontalAlignment(SwingConstants.CENTER);
+		this.textPCargaH.setText("1");
+		this.textPCargaH.setColumns(10);
+		this.panel_5.add(this.textPCargaH);
+
+		this.panel_6 = new JPanel();
+		this.panel_1.add(this.panel_6);
+		this.panel_6.setLayout(new GridLayout(2, 1, 0, 0));
+
+		this.lblNewLabel_11 = new JLabel("");
+		this.panel_6.add(this.lblNewLabel_11);
+
+		this.textPPuesto = new JTextField();
+		this.textPPuesto.addKeyListener(this);
+		this.textPPuesto.setHorizontalAlignment(SwingConstants.CENTER);
+		this.textPPuesto.setText("1");
+		this.textPPuesto.setColumns(10);
+		this.panel_6.add(this.textPPuesto);
+
+		this.panel_7 = new JPanel();
+		this.panel_1.add(this.panel_7);
+		this.panel_7.setLayout(new GridLayout(2, 1, 0, 0));
+
+		this.lblNewLabel_12 = new JLabel("");
+		this.panel_7.add(this.lblNewLabel_12);
+
+		this.textPRangoE = new JTextField();
+		this.textPRangoE.addKeyListener(this);
+		this.textPRangoE.setHorizontalAlignment(SwingConstants.CENTER);
+		this.textPRangoE.setText("1");
+		this.textPRangoE.setColumns(10);
+		this.panel_7.add(this.textPRangoE);
+
+		this.panel_8 = new JPanel();
+		this.panel_1.add(this.panel_8);
+		this.panel_8.setLayout(new GridLayout(2, 1, 0, 0));
+
+		this.lblNewLabel_13 = new JLabel("");
+		this.panel_8.add(this.lblNewLabel_13);
+
+		this.textPExperiencia = new JTextField();
+		this.textPExperiencia.addKeyListener(this);
+		this.textPExperiencia.setHorizontalAlignment(SwingConstants.CENTER);
+		this.textPExperiencia.setText("1");
+		this.textPExperiencia.setColumns(10);
+		this.panel_8.add(this.textPExperiencia);
+
+		this.panel_9 = new JPanel();
+		this.panel_1.add(this.panel_9);
+		this.panel_9.setLayout(new GridLayout(2, 1, 0, 0));
+
+		this.lblNewLabel_14 = new JLabel("");
+		this.panel_9.add(this.lblNewLabel_14);
+
+		this.textPEstudios = new JTextField();
+		this.textPEstudios.addKeyListener(this);
+		this.textPEstudios.setHorizontalAlignment(SwingConstants.CENTER);
+		this.textPEstudios.setText("1");
+		this.textPEstudios.setColumns(10);
+		this.panel_9.add(this.textPEstudios);
+
+		this.lblNewLabel_6 = new JLabel("-");
+		this.lblNewLabel_6.setForeground(UIManager.getColor("Button.background"));
+		this.lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		this.panelPesos.add(this.lblNewLabel_6, BorderLayout.SOUTH);
 		this.setVisible(true);
-		
+
 	}
 
-
-	public void mouseClicked(MouseEvent e) {	
+	public void mouseClicked(MouseEvent e) {
 	}
+
 	public void mouseEntered(MouseEvent e) {
 	}
+
 	public void mouseExited(MouseEvent e) {
 	}
+
 	public void mousePressed(MouseEvent e) {
 	}
+
 	@SuppressWarnings("deprecation")
 	public void mouseReleased(MouseEvent e) {
 		this.btnEnviar.setEnabled(validar());
 	}
-	
+
 	private boolean validar() {
-		boolean resp=false;
-		resp= (rdbtnHomeOffice.isSelected()||rdbtnPresencial.isSelected()||rdbtnIndistinto.isSelected())
-				&& (rdbtnV1.isSelected()||rdbtnV1V2.isSelected()||rdbtnV2.isSelected())
-				&& (rdbtnMedia.isSelected()||rdbtnVCompleta.isSelected()||rdbtnExtendida.isSelected())
-				&& (rdbtnJunior.isSelected()||rdbtnSenior.isSelected()||rdbtnManagement.isSelected())
-				&& (!rdbtnE1.isEnabled()||rdbtnE1.isSelected()||rdbtnE2.isSelected()||rdbtnE3.isSelected())
-				&& (rdbtnNada.isSelected()||rdbtnMucha.isSelected()||rdbtnEMedia.isSelected())
-				&& (rdbtnPrimario.isSelected()||rdbtnSecundario.isSelected()||rdbtnTerciario.isSelected());
+		boolean resp = false;
+		resp = (rdbtnHomeOffice.isSelected() || rdbtnPresencial.isSelected() || rdbtnIndistinto.isSelected())
+				&& (rdbtnV1.isSelected() || rdbtnV1V2.isSelected() || rdbtnV2.isSelected())
+				&& (rdbtnMedia.isSelected() || rdbtnVCompleta.isSelected() || rdbtnExtendida.isSelected())
+				&& (rdbtnJunior.isSelected() || rdbtnSenior.isSelected() || rdbtnManagement.isSelected())
+				&& (!rdbtnE1.isEnabled() || rdbtnE1.isSelected() || rdbtnE2.isSelected() || rdbtnE3.isSelected())
+				&& (rdbtnNada.isSelected() || rdbtnMucha.isSelected() || rdbtnEMedia.isSelected())
+				&& (rdbtnPrimario.isSelected() || rdbtnSecundario.isSelected() || rdbtnTerciario.isSelected())
+				&& !this.textPLocacion.getText().isEmpty() && !this.textPPuesto.getText().isEmpty() 
+				&& !this.textPCargaH.getText().isEmpty() && !this.textPEstudios.getText().isEmpty()
+				&& !this.textPExperiencia.getText().isEmpty() && !this.textPRangoE.getText().isEmpty()
+				&& !this.textPRemuneracion.getText().isEmpty()
+				&& (!this.textCant.isEnabled()|| this.textCant.getText()!=null|| !this.textCant.getText().isEmpty());
 		return resp;
 	}
 
 	public void setActionListener(ActionListener actionListener) {
 		this.btnEnviar.addActionListener(actionListener);
-		this.actionlistener=actionListener;
+		this.actionlistener = actionListener;
 	}
-
 
 	public void cerrarse() {
-		this.dispose();		
+		this.dispose();
 	}
 
+	public void keyPressed(KeyEvent e) {
+	}
+	public void keyReleased(KeyEvent e) {
+	}
+	public void keyTyped(KeyEvent e) {
+	}
 }
