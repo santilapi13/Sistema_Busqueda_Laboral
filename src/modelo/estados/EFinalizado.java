19447401c2f8a2d1bb401775e60b24e160a1017d
Santiago N. Lapiana <santilapiana02@gmail.com>
2022-06-26 23:56:55 -0300

    Informe

--- src/modelo/estados/EFinalizado.java
+++ src/modelo/estados/EFinalizado.java
@@ -14,17 +14,14 @@ public class EFinalizado implements IEstadoTicket,Serializable {
 	
 	@Override
 	public void activarse() {
-		this.ticket.setEstado(new EActivo(this.ticket));
 	}
 
 	@Override
 	public void cancelarse() {
-		this.ticket.setEstado(new ECancelado(this.ticket));
 	}
 
 	@Override
 	public void suspenderse() {
-		this.ticket.setEstado(new ESuspendido(this.ticket));
 	}
 
 	@Override
